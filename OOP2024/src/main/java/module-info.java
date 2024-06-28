475a2b877fab06f7b4b96b88cbdcd7882ae8df85
Moein Yousefinia <69634871+moein-yoo@users.noreply.github.com>
2024-06-28 22:02:55 +0330

    Delete OOP2024 directory
    .

--- OOP2024/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module Pacman {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-    requires java.desktop;
-    exports View;
-    opens View to javafx.fxml;
-}
