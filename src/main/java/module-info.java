d99f69043f67c347fed8634b8cb335ae6fa660fe
Soroush <soroushmomeni11@gmail.com>
2024-06-30 13:37:50 +0330

    1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
-module Pacman {
+module OOP2024 {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
     requires java.desktop;
-    exports View;
-    opens View to javafx.fxml;
+//    exports View;
+//    opens View to javafx.fxml;
 }
