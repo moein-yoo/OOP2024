a7e7ec8f793dda81a39fd85e15bab00aec99b6b8
Soroush <soroushmomeni11@gmail.com>
2024-07-04 17:47:44 +0330

    1

--- src/main/java/Model/MatchData.java
+++ src/main/java/Model/MatchData.java
@@ -12,12 +12,54 @@ public class MatchData {
     String Date;
     int WinnerLevel;
     int LoserLevel;
-    static String url="jdbc:mysql//localHost:3306/matchdata";
+    static String url="jdbc:mysql//localhost:3306/project";
     static String username="root";
     static String password="soroush1384";
     static Connection connection;
-    static Statement statement;
-    public static void initialize(){
-
+    static java.sql.Statement statement;
+    public MatchData(String Winner,String Loser,String Award,String Penalty,String Date,int WinnerLevel,int LoserLevel){
+        this.Winner=Winner;
+        this.Loser=Loser;
+        this.Award=Award;
+        this.Penalty=Penalty;
+        this.Date=Date;
+        this.WinnerLevel=WinnerLevel;
+        this.LoserLevel=LoserLevel;
     }
+    public static void initialize(){
+        matchDatas=new ArrayList<>();
+        try{
+        connection=DriverManager.getConnection(url,username,password);
+        statement=connection.createStatement();
+        }
+        catch (SQLException e){throw new RuntimeException(e);}
+        System.out.println("Database connected");
+        try{
+            ResultSet resultSet=statement.executeQuery("SELECT * FROM MatchData");
+            while(resultSet.next()){
+                String win=resultSet.getString(1);
+                String lose=resultSet.getString(2);
+                String award=resultSet.getString(3);
+                String penalty=resultSet.getString(4);
+                String date=resultSet.getString(5);
+                int winlev=resultSet.getInt(6);
+                int loselev=resultSet.getInt(7);
+                MatchData matchData=new MatchData(win,lose,award,penalty,date,winlev,loselev);
+                matchDatas.add(matchData);
+            }
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+    public String getWinner(){return Winner;}
+
+    public String getLoser() {
+        return Loser;
+    }
+    public String getAward(){return Award;}
+    public String getPenalty(){return Penalty;}
+    public String getDate(){return Date;}
+    public int getWinnerLevel(){return  WinnerLevel;}
+    public int getLoserLevel(){return LoserLevel;}
 }
