11fd3fdd03f5db3f710ab80bf2d67c5b0df2848d
Moein Yousefinia <moein_yoo@outlook.com>
2024-07-11 01:04:20 +0330

    Moein

--- src/main/java/ViewGraphic/HistoryMenuViewController.java
+++ src/main/java/ViewGraphic/HistoryMenuViewController.java
@@ -1,4 +1,32 @@
 package ViewGraphic;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+
 public class HistoryMenuViewController {
+    @FXML
+    public Button awardSort;
+    @FXML
+    public Button PenaltySort;
+    @FXML
+    public Button opponentSort;
+    @FXML
+    public Button hostLevelSort;
+    @FXML
+    public Button oppLevelSort;
+    @FXML
+    public Button resultSort;
+    @FXML
+    public Button timeSort;
+    @FXML
+    public Button nextPageButton;
+    @FXML
+    public Button previousPageButton;
+    @FXML
+    private TableView<String> historyTable;
+
+    public void goBack(MouseEvent mouseEvent) {
+    }
 }
